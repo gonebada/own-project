d81682b280b26e96ec7dca409e296683367c2111
Agnieszka Kot <agnieszkakot22@gmail.com>
2019-07-22 13:53:57 +0200

    poprawa index.html i tabeli

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function toggleMenu(visible) {
-  document.querySelector('.menu').classList.toggle('show', visible)
+  document.querySelector('.side-nav').classList.toggle('show', visible)
 }
 
-document.querySelector('.hamburger').addEventListener('click', function(e) {
+document.querySelector('#nav-icon1').addEventListener('click', function(e) {
   e.preventDefault();
   toggleMenu()
 });
