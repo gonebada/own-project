c1c338d593da789f9b151213d5a60c9e1e126e00
Agnieszka Kot <agnieszkakot22@gmail.com>
2019-08-05 23:35:35 +0200

    formularz do rejestracji-prototyp

--- js/script.js
+++ js/script.js
@@ -1,8 +1,47 @@
-function toggleMenu(visible) {
-  document.querySelector('.side-nav').classList.toggle('show', visible)
-}
+'use strict';
 
-document.querySelector('#nav-icon1').addEventListener('click', function(e) {
-  e.preventDefault();
-  toggleMenu()
-});
+//var resizeNav = document.getElementById('menu-arrow');
+//resizeNav.addEventListener('click', function () {
+//    size();
+//}
+//function size(){
+//    document.getElementById('nav-1').style.height="400px";
+//}
+//
+//
+//function myFunction() {
+//  document.getElementById("myDropdown").classList.toggle("show");
+//}
+//
+//// Close the dropdown if the user clicks outside of it
+//window.onclick = function(event) {
+//  if (!event.target.matches('.dropbtn')) {
+//    var dropdowns = document.getElementsByClassName("dropdown-content");
+//    var i;
+//    for (i = 0; i < dropdowns.length; i++) {
+//      var openDropdown = dropdowns[i];
+//      if (openDropdown.classList.contains('show')) {
+//        openDropdown.classList.remove('show');
+//      }
+//    }
+//  }
+//}
+
+
+function resize(select, size) {
+    var nav = document.querySelector(select);
+    var subMenu=document.querySelector('div.sub-menu');
+    if (nav.style.height == '40px') {
+        nav.style.height = size;
+        subMenu.css("display","block");
+    } else {
+        nav.style.height = '40px';
+    }
+};
+
+$('#toggleBtn').click(function(){
+    if ($window).width()>500){
+        $
+    }
+})
+//https://www.w3schools.com/howto/howto_js_dropdown.asp
