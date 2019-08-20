8092ae5645eaafc2787764cb569bda349fe71eaa
Agnieszka Kot <agnieszkakot22@gmail.com>
2019-08-21 01:44:31 +0200

    mobile navbar + mobile sidebar, style for mobile

--- js/script.js
+++ js/script.js
@@ -1,69 +1,30 @@
 'use strict';
 
-//var resizeNav = document.getElementById('menu-arrow');
-//resizeNav.addEventListener('click', function () {
-//    size();
-//}
-//function size(){
-//    document.getElementById('nav-1').style.height="400px";
-//}
-//
-//
-//function myFunction() {
-//  document.getElementById("myDropdown").classList.toggle("show");
-//}
-//
-//// Close the dropdown if the user clicks outside of it
-//window.onclick = function(event) {
-//  if (!event.target.matches('.dropbtn')) {
-//    var dropdowns = document.getElementsByClassName("dropdown-content");
-//    var i;
-//    for (i = 0; i < dropdowns.length; i++) {
-//      var openDropdown = dropdowns[i];
-//      if (openDropdown.classList.contains('show')) {
-//        openDropdown.classList.remove('show');
-//      }
-//    }
-//  }
-//}
+//// Navbar menu
+const menuArrow = document.getElementById('mobile');
+const navMenu =  document.getElementById('nav-1')
+const navSubMenu =document.querySelector('.sub-menu');
+menuArrow.addEventListener('click', () => {
+    navMenu.classList.toggle('resizeNav')
+    navSubMenu.classList.toggle('resizeNav');
+});
 
 
-function resize(select, size) {
-    var nav = document.querySelector(select);
-    var subMenu = document.querySelector('div.sub-menu');
-    if (nav.style.height == '40px') {
-        nav.style.height = size;
-        subMenu.css("display", "block");
-    } else {
-        nav.style.height = '40px';
-    }
-};
+/// ===== > Open / close sidebar <==== ///
+const showSidebar = document.getElementById('showSidebar');
+const sideBar = document.querySelector('.sidebar');
+const sidebarHide = document.getElementById('sidebarHide');
+showSidebar.addEventListener('click', () => {
+    sideBar.classList.add('show-sidebar');
+})
 
-//$('#toggleBtn').click(function () {
-//    if ($window).width() > 500) {
-//    $
-//}
-//})
-////https://www.w3schools.com/howto/howto_js_dropdown.asp
-
-
-//FORMULARZE WALIDACJA
-//var usrName = document.getElementById('Username').value;
-//usrName.addEventListener('input', function (event) {
-//    if (usrName.validity.typeMismatch) {
-//        usrName.setCustomValidity('Name must be filled out');
-//    } else {
-//        usrName.setCustomValidity('');
-//    }
-//})
-//document('div.form-group').addEventListener('submit',
-//    function (event) {
-//        var isFormValidate = true;
-//
-//    })
+sidebarHide.addEventListener('click', () => {
+    sideBar.classList.remove('show-sidebar');
+})
 
 //Modale
-var btn=document.getElementById('modalLogIn');
+var btn = document.getElementById('modalLogIn');
+
 function closeModal() {
     document.getElementById('overlay').classList.remove('show')
 }
@@ -87,24 +48,18 @@ document.addEventListener('keyup', function (e) {
 })
 
 ////Open selected modal. close other.
-function openModal(modal){
-    document.querySelectorAll('#overlay >*').forEach(function(modal){
+function openModal(modal) {
+    document.querySelectorAll('#overlay >*').forEach(function (modal) {
         modal.classList.remove('show')
     })
     document.querySelector('#overlay').classList.add('show')
     document.querySelector(modal).classList.add('show')
 }
 
-
-
-
-
-
-
 //---> CHART <----///
 var ctx = document.getElementById('myChart').getContext('2d');
 
- var chart = new Chart(ctx, {
+var chart = new Chart(ctx, {
     // 1
     type: 'bar', //column type
     data: {
@@ -112,27 +67,27 @@ var ctx = document.getElementById('myChart').getContext('2d');
         labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
         // 3
         datasets: [{
-            // 4
-            label: "Signups",
-            // 5
-            backgroundColor: '#8DBEC8',
-            borderColor: '#8DBEC8',
-            // 6
-            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+                // 4
+                label: "Signups",
+                // 5
+                backgroundColor: '#8DBEC8',
+                borderColor: '#8DBEC8',
+                // 6
+                data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
         },
-        {
-            label: "FTD",
-            backgroundColor: '#F29E4E',
-            borderColor: '#F29E4E',
-            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+            {
+                label: "FTD",
+                backgroundColor: '#F29E4E',
+                borderColor: '#F29E4E',
+                data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76],
         },
-        {
-            label: "Earned",
-            backgroundColor: '#71B374',
-            borderColor: '#71B374',
-            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
-            // 7
-            hidden: true,
+            {
+                label: "Earned",
+                backgroundColor: '#71B374',
+                borderColor: '#71B374',
+                data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
+                // 7
+                hidden: true,
         }]
     },
 });
